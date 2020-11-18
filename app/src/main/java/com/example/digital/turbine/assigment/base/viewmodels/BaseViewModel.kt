55f3debc3fa74bf90fc257e0bdaded2533dde43a
joNA <jitzian@gmail.com>
2020-11-18 15:31:37 -0500

    dt jc - Setting up inheritance for ShowAdsViewModel. Also adding foundational work for calling rest API

--- app/src/main/java/com/example/digital/turbine/assigment/base/viewmodels/BaseViewModel.kt
+++ app/src/main/java/com/example/digital/turbine/assigment/base/viewmodels/BaseViewModel.kt
@@ -1,9 +1,11 @@
 package com.example.digital.turbine.assigment.base.viewmodels
 
 import androidx.lifecycle.ViewModel
+import com.example.digital.turbine.assigment.rest.RestApi
 import java.util.logging.Logger
 
 abstract class BaseViewModel : ViewModel() {
     protected lateinit var TAG: String
     protected lateinit var logger: Logger
+    protected lateinit var restApi: RestApi
 }
---
+++ app/src/main/java/com/example/digital/turbine/assigment/showAds/viewmodels/ShowAdsViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.digital.turbine.assigment.showAds.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.digital.turbine.assigment.base.viewmodels.BaseViewModel
+import com.example.digital.turbine.assigment.dagger.components.DaggerComponentInjector
+import com.example.digital.turbine.assigment.dagger.modules.RetrofitModule
+import com.example.digital.turbine.assigment.rest.RestApi
+import com.example.digital.turbine.assigment.rest.model.Ad
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import java.util.logging.Logger
+import javax.inject.Inject
+
+class ShowAdsViewModel : BaseViewModel() {
+    private val injector = DaggerComponentInjector.builder()
+        .retrofitModule(RetrofitModule())
+        .build()
+
+    @Inject
+    lateinit var retrofit: Retrofit
+
+    init {
+        TAG = ShowAdsViewModel::class.java.simpleName
+        logger = Logger.getLogger(TAG)
+        inject()
+    }
+
+    private fun inject() {
+        injector.inject(this)
+        restApi = retrofit.create(RestApi::class.java)
+        fetchRemoteData()
+    }
+
+    private val listOfAds: MutableLiveData<List<Ad>> by lazy {
+        MutableLiveData<List<Ad>>()
+    }
+
+    fun getLisOfAds(): LiveData<List<Ad>> {
+        return listOfAds
+    }
+
+    fun fetchRemoteData() = viewModelScope.launch(Dispatchers.IO) {
+
+//        listOfAds.postValue(restApi.getAds().execute().body()?.listOfAds)
+        val result = restApi.getAds().execute().body()
+        Log.e(TAG, "fetchRemoteData::${result?.listOfAds?.size}")
+
+
+    }
+
+}
