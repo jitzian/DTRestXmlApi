c1d0a07c944855d75c2bd20319c57cb71d969a66
joNA <jitzian@gmail.com>
2020-11-19 14:18:23 -0500

    dt jc - Removing commented code, but still I need to optimize the URL

--- app/src/main/java/com/example/digital/turbine/assigment/rest/RestApi.kt
+++ app/src/main/java/com/example/digital/turbine/assigment/rest/RestApi.kt
@@ -7,6 +7,6 @@ import retrofit2.http.GET
 interface RestApi {
 
     //TODO: Change
-    @GET("getAds?id=236&password=OVUJ1DJN&siteId=10777&deviceId=4230&sessionId=techtestsession&totalCampaignsRequested=3")
+    @GET("getAds?id=236&password=OVUJ1DJN&siteId=10777&deviceId=4230&sessionId=techtestsession&totalCampaignsRequested=10")
     fun getAds(): Call<Ads>
 }
